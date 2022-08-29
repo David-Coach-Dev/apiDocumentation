5ca046bba4597965811c004ad3dc00d68613a4b2
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2022-08-29 00:13:11 -0500

    act pag 404

--- src/pages/Pag404/Pag404.tsx
+++ src/pages/Pag404/Pag404.tsx
@@ -1,10 +1,21 @@
-import { Login } from "../Login"
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { PrivateRoutes } from "../../model";
+import { resetUser, UserKey } from "../../redux/states/user"
+import { clearLocalStorage } from "../../utilities"
 
 function Pag404() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const reDir= () => {
+    clearLocalStorage(UserKey);
+    dispatch(resetUser());
+    navigate(`/${PrivateRoutes.PRIVATE}`, {replace: true});
+  }
   return (
     <div>
       <h2>Pag Not Found Error 404.</h2>
-      <Login />
+      <button onClick={reDir}>Regresar</button>
     </div>
 
   )
